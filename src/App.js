1ea645672e6ece0b1b362eaaaf08a214a42148db
Theoutrace <prakashkumar.imw@gmail.com>
2024-03-23 01:09:19 +0530

    -- added text dynamically

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [searchParams] = useSearchParams();
+  const [clientName, setClientName] = useState("");
+
+  useEffect(() => {
+    setClientName(searchParams.get("client").toUpperCase());
+  }, [searchParams]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>Hello</h1>
+          <h5>from {clientName}</h5>
+        </div>
       </header>
     </div>
   );
